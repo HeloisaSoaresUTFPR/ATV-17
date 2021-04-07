2f428fd50360c1fd9d2767f139924027b96a06d3
HeloisaSoaresUtpr <helo_soarescosta@hotmail.com>
2021-04-07 13:19:04 -0300

    Novo livro, livro existente

---
+++ client.js
@@ -0,0 +1,41 @@
+const axios = require('axios').default;
+
+
+axios.post('http://localhost:3000/books', {
+    ID: 3,
+    name: 'My Boook',
+    author: 'Me and only me'
+})
+    .then((res) => {
+        console.log(res.data);
+    })
+    .catch(err => {
+        console.log(err.response.data);
+    });
+
+axios.post('http://localhost:3000/books',{
+    ID: 4,
+    name: 'Novo Book',
+    author: 'HELOISA'
+}).then(resposta => {
+    console.log(resposta.data)
+}).catch(erro => {
+    console.log(erro.response.data);
+});
+
+axios.post('http://localhost:3000/books',{
+    ID: 1,
+    name: 'Novo Book',
+    author: 'Me and only mee'
+}).then(resposta => {
+    console.log(resposta.data)
+}).catch(erro => {
+    console.log(erro.response.data);
+});
+
+axios.get('http://localhost:3000/books')
+    .then((response) => {
+        console.log(response.data);
+    });
+
+
